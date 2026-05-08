868a8c5c01780dfc78a913341cf115de6232063c
Sudarmani <bsudarmani2006@gmail.com>
2026-05-08 21:57:51 +0530

    change

---
+++ FindDigit1/FindDigit1/Program.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Linq;
+//namespace FindDigit1
+//{
+//    class Program
+//    {
+//        Console.Write("hello");
+//    }
+//}
+
+using System;
+using System.Security.Cryptography;
+
+public class FindDigit1
+{
+    
+    public static void Main(string[] args)
+    {
+        
+    }
+}
